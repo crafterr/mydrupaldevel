3697802c2c107f2eb876fc2782fa284000a9dcca
Adam Pietras <crafter@3510344.no-reply.drupal.org>
2019-05-13 19:32:51 +0200

    translator

--- web/modules/custom/hello_world/src/Controller/TranslatorController.php
+++ web/modules/custom/hello_world/src/Controller/TranslatorController.php
@@ -13,13 +13,44 @@ class TranslatorController extends ControllerBase {
     $accountProxy = \Drupal::currentUser();
     $user = \Drupal::entityTypeManager()->getStorage('user')->load($accountProxy->id());
 
-   // dump($userName); die();
+    $url = 'http://www.onet.pl';
 
+    /**
+     * :variable = Use this style of placeholder when substituting the value of an href attribute. Values will be HTML escaped and filtered for dangerous protocols.
+     */
+    $urlLink = t('Hello <a href=":url">@name</a>', array(':url' => 'http://example.com', '@name' => 'Adam'));
+
+
+    /**
+     * %variable
+     */
+
+    /**
+     * Use this style of placeholder to pass text through drupal_placeholder() which will result in the text being HTML escaped, and then wrapped with <em> tags.
+     */
+    /**
+     * output
+     * The file was saved to <em class="placeholder">sites/default/files/myfile.txt</em>.
+     */
+
+    $path_to_file = 'sites/default/files/myfile.txt';
+    t('The file was saved to %path.', array('%path' => $path_to_file));
+
+
+
+
+
+    // dump($userName); die();
+
+    //:variable
+    /**
+     * t('Hello <a href=":url">@name</a>', array(':url' => 'http://example.com', '@name' => $name));
+     */
     $title = t('@name Blog', array('@name' => $user->getDisplayName()));
 
     return [
       '#type' => 'markup',
-      '#markup' => $title
+      '#markup' => $urlLink
     ];
   }
 
